97a51bf576135c98cf875edd34968e5c27a55844
peggy <107b11737@mailst.cjcu.edu.tw>
2020-05-06 11:08:47 +0800

    recyclerView終於完成了(開心 撒花～

--- app/src/main/java/com/example/test_bmi/BMI_List.java
+++ app/src/main/java/com/example/test_bmi/BMI_List.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -22,18 +23,23 @@ public class BMI_List extends AppCompatActivity {
         String h = bundle.getString("height");
         String w = bundle.getString("weight");
         String bmi = bundle.getString("bmi");
+        item_list a = new item_list();
+        a.setBmi(bmi);
+        a.setHeight(h);
+        a.setWeight(w);
+
 
         ArrayList<item_list>list = new ArrayList<>();
+
+        list.add(a);
+        list.add(a);
+        list.add(a);
+        Log.d("aa",String.valueOf(list.size()));
         bmi_list = (RecyclerView) findViewById(R.id.bmi_list);
         BMIAdapter adapter = new BMIAdapter(list);
         bmi_list.setAdapter(adapter);
         bmi_list.setLayoutManager(new LinearLayoutManager(this));
 
-        item_list a = new item_list();
-        a.setBmi(bmi);
-        a.setBmi(h);
-        a.setBmi(w);
-        list.add(a);
 
     }
 }
--- app/src/main/java/com/example/test_bmi/item_list.java
+++ app/src/main/java/com/example/test_bmi/item_list.java
@@ -1,29 +1,17 @@
 package com.example.test_bmi;
 
 public class item_list {
-    public String getHeight() {
-        return height;
-    }
+    public String getHeight() { return height; }
 
-    public void setHeight(String height) {
-        this.height = height;
-    }
+    public void setHeight(String height) { this.height = height; }
 
-    public String getWeight() {
-        return weight;
-    }
+    public String getWeight() { return weight; }
 
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
+    public void setWeight(String weight) { this.weight = weight; }
 
-    public String getBmi() {
-        return bmi;
-    }
+    public String getBmi() { return bmi; }
 
-    public void setBmi(String bmi) {
-        this.bmi = bmi;
-    }
+    public void setBmi(String bmi) { this.bmi = bmi; }
 
     String height;
     String weight;
